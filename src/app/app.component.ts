e843959e60a3571b9337687081d8fd30231bd7d8
Williams Aguilera <williams.aguilera@cloud-iq.com>
2019-06-18 17:51:36 +0200

    MOD: Obtenemos series a partir del servicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TvShow } from './tv-show';
+import { ShowServiceService } from './show-service.service';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +9,11 @@ import { TvShow } from './tv-show';
 })
 export class AppComponent {
 
-  public series: Array<TvShow> = [{
-    nombre: 'Breaking Bad',
-    temporadas: 5,
-    episodios: 62
-  }, {
-    nombre: 'Game of Thrones',
-    temporadas: 8,
-    episodios: 73
-  }, {
-    nombre: 'Lucifer',
-    temporadas: 4,
-    episodios: 67
-  }]
+  public series: Array<TvShow>;
+
+  constructor(showService: ShowServiceService) {
+    this.series = showService.obtenerSeries();
+  }
 
 
   public modificarSerie(nombre: string, temporadas: number, episodios: number): void {
--- src/app/show-service.service.ts
+++ src/app/show-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { TvShow } from './tv-show';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,20 @@ import { Injectable } from '@angular/core';
 export class ShowServiceService {
 
   constructor() { }
+
+  public obtenerSeries(): Array<TvShow> {
+    return [{
+      nombre: 'Breaking Bad',
+      temporadas: 5,
+      episodios: 62
+    }, {
+      nombre: 'Game of Thrones',
+      temporadas: 8,
+      episodios: 73
+    }, {
+      nombre: 'Lucifer',
+      temporadas: 4,
+      episodios: 67
+    }];
+  }
 }
