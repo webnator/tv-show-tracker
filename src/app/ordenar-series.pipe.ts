5cbfc407a921dff973cd005e274986e8c66cc583
Williams Aguilera <williams.aguilera@cloud-iq.com>
2019-06-18 18:14:44 +0200

    MOD: Añadido pipe para ordenar series

--- src/app/ordenar-series.pipe.ts
+++ src/app/ordenar-series.pipe.ts
@@ -1,12 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TvShow } from './tv-show';
 
 @Pipe({
   name: 'ordenarSeries'
 })
 export class OrdenarSeriesPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(listaSeries: Array<TvShow>, args?: any): any {
+    listaSeries.sort((a: TvShow, b: TvShow) => {
+      if (a.rate > b.rate) {
+        return -1;
+      } else if (a.rate < b.rate) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return listaSeries;
   }
 
 }
