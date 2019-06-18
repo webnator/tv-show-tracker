d695ffe7b97dfd4d6a92dce7e1b6b820f4103c8f
Williams Aguilera <williams.aguilera@cloud-iq.com>
2019-06-18 17:34:28 +0200

    MOD: ShowDetails ahora muestra detalles de la serie

--- src/app/show-details/show-details.component.ts
+++ src/app/show-details/show-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TvShow } from '../tv-show';
 
 @Component({
   selector: 'app-show-details',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-details.component.sass']
 })
 export class ShowDetailsComponent implements OnInit {
+
+  public serie: TvShow = {
+    nombre: 'Breaking Bad',
+    temporadas: 5,
+    episodios: 62
+  }
 
   constructor() { }
 
