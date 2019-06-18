286d9996fb101b66b38f111cfbd7ade9c6c72e0e
Williams Aguilera <williams.aguilera@cloud-iq.com>
2019-06-18 17:46:36 +0200

    MOD: Modificados estilos del componente

--- src/app/show-details/show-details.component.ts
+++ src/app/show-details/show-details.component.ts
@@ -4,7 +4,7 @@ import { TvShow } from '../tv-show';
 @Component({
   selector: 'app-show-details',
   templateUrl: './show-details.component.html',
-  styleUrls: ['./show-details.component.sass']
+  styleUrls: ['./show-details.component.scss']
 })
 export class ShowDetailsComponent implements OnInit {
 
