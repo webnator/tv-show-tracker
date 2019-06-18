f593ac2236f23b32bbe5073aab572bf08628420a
Williams Aguilera <williams.aguilera@cloud-iq.com>
2019-06-18 17:39:05 +0200

    MOD: Creada la variable series con 3 series

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,23 @@ import { TvShow } from './tv-show';
 })
 export class AppComponent {
 
-  public serie: TvShow = {
+  public series: Array<TvShow> = [{
     nombre: 'Breaking Bad',
     temporadas: 5,
     episodios: 62
-  }
+  }, {
+    nombre: 'Game of Thrones',
+    temporadas: 8,
+    episodios: 73
+  }, {
+    nombre: 'Lucifer',
+    temporadas: 4,
+    episodios: 67
+  }]
 
 
   public modificarSerie(nombre: string, temporadas: number, episodios: number): void {
-    this.serie = {
+    this.series[0] = {
       nombre,
       temporadas,
       episodios
