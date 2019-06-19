3c8b7f87d8a13897162d5fa16594ade5f99d2d70
Williams Aguilera <webnator@gmail.com>
2019-06-19 19:45:23 +0200

    ADD: añadido ruteo basico de la pagina; ADD: extraido agregar serie a nueva pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddShowComponent } from './add-show/add-show.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'agregar-serie', component: AddShowComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
