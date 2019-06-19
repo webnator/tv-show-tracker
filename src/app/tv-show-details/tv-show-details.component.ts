e6df6765aede492740c927b4461ba022d9292262
Williams Aguilera <webnator@gmail.com>
2019-06-19 20:21:45 +0200

    ADD: añadido boton de atras

--- src/app/tv-show-details/tv-show-details.component.ts
+++ src/app/tv-show-details/tv-show-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShowServiceService } from '../show-service.service';
 import { TvShow } from '../tv-show';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-tv-show-details',
@@ -11,11 +12,15 @@ import { ActivatedRoute } from '@angular/router';
 export class TvShowDetailsComponent implements OnInit {
   public serie: TvShow;
 
-  constructor(private showService: ShowServiceService, private route: ActivatedRoute) {}
+  constructor(private showService: ShowServiceService, private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
     const indice = this.route.snapshot.paramMap.get('indice');
     this.serie = this.showService.obtenerSerie(parseInt(indice));
+  }
+
+  public volver(): void {
+    this.location.back();
   }
 
 }
